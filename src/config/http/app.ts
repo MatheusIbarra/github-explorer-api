a3e6b66cb6568dc67be1b307d3844eb1342c7227
Matheus <matheus.ibarra@wapstore.com.br>
2023-10-12 18:49:50 -0300

    fix: ci - build

--- src/config/http/app.ts
+++ src/config/http/app.ts
@@ -5,12 +5,15 @@ import { setupHeaders } from './headers';
 import { setupMiddlewares } from './middlewares';
 import { setupRoutes } from './routes';
 import { setupSwagger } from './swagger';
+import { corsSetup } from './middlewares/corsSteup';
 
 export async function getExpressApp() {
   const app = express();
+
   setupHeaders(app);
   setupSwagger(app);
   setupMiddlewares(app);
+  app.use(corsSetup);
   await setupRoutes(app);
 
   return app;
